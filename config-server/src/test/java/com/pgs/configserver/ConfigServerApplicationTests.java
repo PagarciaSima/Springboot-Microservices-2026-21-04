262a8c6ad23228f6b19768c1a80a3b53377f4f41
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-04-21 06:48:34 +0200

    config server & discovery server

---
+++ config-server/src/test/java/com/pgs/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
